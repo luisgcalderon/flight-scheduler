c45638f0f2587f8bf7bb47ed76f28024a4a1477b
Luis Calderon <luis.calderon@anahuac.mx>
2020-10-03 17:40:13 -0500

    Add of flight list componenet and modules of HttpClient and FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { FlightListComponent } from './flight-list/flight-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClient
+
   ],
   providers: [],
   bootstrap: [AppComponent]
